fe606975cda462f3e4ac8791cb861f38e7653053
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-01-20 18:47:47 +0100

    Refactor and doc

--- 05_BBDD/entrega/helpers.php
+++ 05_BBDD/entrega/helpers.php
@@ -4,7 +4,7 @@ function carga($clase)
 {
     require "$clase.php";
 }
-
+session_start();
 spl_autoload_register("carga");
 
 if ($_SESSION['usuario'] == null) {
--- 05_BBDD/entrega/insert.php
+++ 05_BBDD/entrega/insert.php
@@ -48,7 +48,7 @@ if (isset($_POST['table'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Insertar - </title>
+    <title>Insertar</title>
     <link rel='stylesheet' href='https://unpkg.com/@picocss/pico@latest/css/pico.min.css'>
     <link rel="stylesheet" href="assets/style.css">
 
