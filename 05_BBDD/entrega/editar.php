899b78d2a8bcf74c150a6b6de1d67df523b770a7
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-01-20 16:14:53 +0100

    add editar.php

---
+++ 05_BBDD/entrega/editar.php
@@ -0,0 +1,27 @@
+<?php
+//Declaro las variables
+$msj = null;
+$form = null;
+
+require "helpers.php";
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar - </title>
+<link rel='stylesheet' href='https://unpkg.com/@picocss/pico@latest/css/pico.min.css'>
+<link rel="stylesheet" href="assets/style.css">
+
+</head>
+<body>
+    <form action="editar.php" method="post">
+
+    </form>
+</body>
+</html>
