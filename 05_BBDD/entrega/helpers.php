a1cb92440460c1275fa3ef51f82fc4997e2dd2f1
Carlos Sesma <41452910+usarral@users.noreply.github.com>
2023-01-20 16:13:24 +0100

    Force login

--- 05_BBDD/entrega/helpers.php
+++ 05_BBDD/entrega/helpers.php
@@ -6,3 +6,11 @@ function carga($clase)
     require "$clase.php";
 }
 spl_autoload_register("carga");
+
+session_start();
+
+// Si el usuario no esta logueado
+if ($_SESSION['usuario'] == null) {
+    header("Location: ./login.php");
+    exit;
+}
